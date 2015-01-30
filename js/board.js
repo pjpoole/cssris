3698e5dedb6fb069e584e894f85e8823001ae345
Peter Poole <peter.j.poole@gmail.com>
2015-01-29 21:34:48 -0500

    Board infrastructure

--- js/board.js
+++ js/board.js
@@ -3,9 +3,14 @@ if (typeof CSSris === "undefined") {
   window.CSSris = {};
 }
 
-
 // This is where the logic for the game gets stored and interpreted.
 var Board = CSSris.Board = function () {
+  // TODO: save highscore
+  this.clearBoard();
+
+  this.highscore = 0;
+  this.points = 0;
+  this.lines = 0;
 
 };
 
@@ -13,6 +18,72 @@ var Board = CSSris.Board = function () {
 Board.X_DIM = 10;
 Board.Y_DIM = 20;
 
+Board.prototype.die = function () {
+  this.highscore = Math.max(this.points, this.highscore);
+  this.points = 0;
+  this.lines = 0;
+
+  this.clearBoard();
+};
+
+Board.prototype.clearBoard = function () {
+  var x, y;
+
+  for (x = 0; x < Board.X_DIM; x++) {
+    for (y = 0; y < Board.Y_DIM; y++) {
+      this.set([x, y]) = false;
+    }
+  }
+};
+
+Board.prototype.get = function (x, y) {
+  if (this.inbounds(x, y)) {
+    return this._grid[x][y];
+  }
+  return;
+},
+
+Board.prototype.set = function (x, y, val) {
+  if (this.inbounds(x, y)) {
+    this._grid[x][y] = val;
+  }
+},
+
+Board.prototype.inbounds = function (x, y) {
+  return ((x > 0 && x < Board.X_DIM) && (y > 0 && y < Board.Y_DIM));
+};
+
+Board.prototype.grid = function () {
+  var x;
+
+  if (!this._grid) {
+    this._grid = new Array(Board.X_DIM);
+    for (x = 0; x < Board.X_DIM; x++) {
+      this._grid[x] = new Array(Board.Y_DIM);
+    }
+  }
+};
+
+Board.prototype.render = function () {
+  var val, x, y;
+
+  str = "";
+
+  for (y = 0; y < Board.Y_DIM; y++) {
+    for (x = 0; x < Board.X_DIM; x++) {
+      val = this.get(x, y);
+
+      if (val) {
+        str += "+";
+      } else {
+        str += " ";
+      }
+    }
+    str += "\n";
+  }
+
+  return str;
+};
 
 
 });
---
+++ js/piece.js
@@ -0,0 +1,22 @@
+(function () {
+if (typeof CSSris === "undefined") {
+  window.CSSris = {};
+}
+
+var Piece = CSSris.Piece = function (board) {
+  this.board = board;
+
+};
+
+Piece.prototype.move = function (dir) {
+
+};
+
+Piece.prototype.rotate = function (dir) {
+
+};
+
+Piece.prototype.drop = function () {
+  
+};
+})();
