87c255af5bb56218aac671ab886fe9c464c44158
Peter Poole <peter.j.poole@gmail.com>
2015-01-30 19:42:28 -0500

    Pieces detect walls correctly

--- js/piece.js
+++ js/piece.js
@@ -86,7 +86,6 @@ Piece.prototype.randomPiece = function () {
 
 Piece.prototype.step = function () {
   this.drop();
-  console.log(this.pos);
 };
 
 Piece.prototype.update = function (callback) {
@@ -130,10 +129,9 @@ Piece.prototype.rotate = function (dir) {
 };
 
 Piece.prototype.move = function (dir) {
-  console.log(dir)
   var offset = dir === 'L' ? -1 : 1,
       callback = function (offs) {
-        return [this.pos[0] + offset, this.pos[1]];
+        return [this.pos[0] + offs[0] + offset, this.pos[1] + offs[1]];
       },
       tempPiece = this.currentPiece(callback);
 
@@ -154,10 +152,8 @@ Piece.prototype.drop = function () {
   if (this.testPos(tempPiece)) {
     this.update(function () {
       this.pos = newPos;
-      console.log(this.pos)
     }.bind(this));
   } else {
-    console.log('stopped')
     this.stop();
   }
 };
